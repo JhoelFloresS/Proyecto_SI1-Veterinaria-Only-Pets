93258db404f93479ef5fb2e9d40d49b965dbc971
Johnny Sucre <johnnysucre8@gmail.com>
2022-05-22 02:20:46 -0400

    Intetando Subir Register :v

--- resources/views/register.php
+++ resources/views/register.php
@@ -5,7 +5,7 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/fontawesome.min.css">
-    <title>login</title>
+    <title>Registro</title>
 
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;1,100;1,600&display=swap');
@@ -103,15 +103,18 @@
             margin-top: 40px;
         }
 
+        .nombre,
+        .apellido,
         .email,
-        .contraseña {
+        .contraseña,
+        .texto2 {
             color: black;
             margin-top: 18px;
             font-weight: 600;
             font-size: 15px;
         }
 
-        .buttonIniciarSesion {
+        .buttonRegistrame {
             font-size: 20px;
             font-weight: 700;
 
@@ -126,9 +129,22 @@
             margin-left: auto;
             margin-right: auto;
         }
+        .buttonTengoCuenta{
+            font-size: 20px;
+            font-weight: 700;
+            text-align: center;
+
+            width: 250px;
+            padding: 13px;
+
+             /* centrando el boton */
+             display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
 
         /* sombreado de los botones , color de letra , borde y color del contorno*/
-        .buttonIniciarSesion {
+        .buttonRegistrame {
             /* color contorno*/
             background: #2FE276;
             /* color de la letra*/
@@ -138,6 +154,20 @@
             /*borde*/
             border-radius: 10px;
             border: none;
+        }
+
+        .buttonTengoCuenta {
+            /* color contorno*/
+            background:#fff;
+            /* color de la letra*/
+            color:#2FE276;
+            /* sombreado*/
+            box-shadow: 0px 10px 20px rgba(47, 226, 118, 0.41);
+            /*borde*/
+            border-radius: 10px;
+            border: none;
+
+            margin-top: 20px;
         }
 
         .myInput {
@@ -176,30 +206,48 @@
                 <div class="col-md-6">
                     <div class="rectanguloIzquierdo">
                         <div class="textIniciarSesion text-center">
-                            <header>Iniciar Sesion</header>
+                            <header>Crear una cuenta</header>
                         </div>
 
-                        <div class="email">Email</div>
+                        <div class="nombre">Nombre</div>
+
+                        <div class="form-group">
+                            <input class="myInput" placeholder="Ingresa tu Nombre" type="text" id="nombre" required>
+                        </div>
+
+                        <div class="apellido">Apellido</div>
+
+                        <div class="form-group">
+                            <input class="myInput" placeholder="Ingresa tu Apellido" type="text" id="apellido" required>
+                        </div>
+
+                        <div class="email">email</div>
 
                         <div class="form-group">
                             <input class="myInput" placeholder="Ingresa tu email" type="text" id="email" required>
                         </div>
 
                         <div class="contraseña">Contraseña</div>
-
+                        
                         <div class="form-group">
                             <input class="myInput" type="password" id="password" placeholder="Ingresa tu contraseña" required>
                         </div>
 
-                        <input type="submit" class="buttonIniciarSesion" value="Iniciar Sesión">
+                        <div class="contraseña">Confirmar contraseña</div>
 
-                        <div class="texto text-center">
-                            <header>¿Todavia no tienes una cuenta?</header>
+                        <div class="form-group">
+                            <input class="myInput" type="password" id="Conf_password" placeholder="Repite tu contraseña" required>
                         </div>
-                        <div class = "texto1 text-center">
-                            <a class="texto_registro" href="http://127.0.0.1:8000/register">Registrate aqui</a>
+
+
+                        <div class="texto2 text-center">
+                            <header>Acepto los terminos y condiciones del servicio</header>
                         </div>
-                    </div> 
+
+                        <input type="submit" class="buttonRegistrame" value="Registrarme">
+                        <a type='button'class="buttonTengoCuenta" href="http://127.0.0.1:8000/login">Ya tengo cuenta</a>                      
+
+                    </div>
                 </div>
                 <div class="col-md-6">
                     <div class="rectanguloDerecho">
