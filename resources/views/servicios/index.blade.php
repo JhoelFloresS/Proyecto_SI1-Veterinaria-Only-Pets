33e8a667946c40f12c9f395fb28c45b820171453
MarcoToledo <marcodavidtoledo@gmail.com>
2022-06-11 23:32:00 -0400

    Terminé mi issue, siuuu

--- resources/views/servicios/index.blade.php
+++ resources/views/servicios/index.blade.php
@@ -3,14 +3,21 @@
 @section('servicio','active')
 @section('css-derecha')
 <link rel="stylesheet" href="{{asset('css/table-information.css')}}">
+<style>
+  #ServicioFormUpdate .form-control,
+  #ServicioFormUpdate .form-select,
+  #ServicioFormUpdate .select2-selection {
+    background-color: khaki !important;
+  }
+</style>
 @endsection
 @section('contenido')
 
 
 <div class="registrar">
-  <a href="" class="buttonRegistrame">
-    Registrar <br>Usuario
-  </a>
+  <button class="buttonRegistrame" data-bs-toggle="modal" data-bs-target="#ServicioFormInput" onclick="createSelector('servicio','input')">
+    Registrar <br>Servicio
+  </button>
 </div>
 <div class="d-md-flex justify-content-md-end" style="margin-bottom: 1rem;">
   <form action="{{ route('servicios.index')}}" method="GET">
@@ -37,13 +44,12 @@
         <td>{{$servicio->id}}</td>
         <td>{{$servicio->nombre}}</td>
         <td>{{$servicio->precio}}</td>
-        <td>
-          <a href=" " class="button-edit">
-            <span class="material-icons-sharp">
+        <td><button class="button-edit">
+            <span class="material-icons-sharp" onclick=@php echo "\" imprimir(" . json_encode($servicio->id) . ")\""; @endphp data-bs-toggle="modal"
+              data-bs-target="#ServicioFormUpdate">
               edit
             </span>
-          </a>
-        </td>
+          </button></td>
         <td>
           <a href="{{route('servicios.show', $servicio)}}" class="button-edit" id="ver">
             <span class="material-icons-sharp">
@@ -61,4 +67,33 @@
 </div>
 
 
+@endsection
+
+@section('body-final')
+<x-forms.input-datos-servicios id="ServicioFormInput" type="servicio" />
+<x-forms.update-datos-servicios id="ServicioFormUpdate" type="servicio" />
+@endsection
+@section('js-home')
+
+<script>
+  //EVENTO ONCLICK PARA EL BOTON DE EDITAR
+  function imprimir(id) {
+    var admin = new XMLHttpRequest()
+    admin.open("GET", "/servicios/datas/" + id.toString(), true)
+    admin.addEventListener("load", cargarDatos)
+    admin.send()
+  }
+
+  function cargarDatos(e) {
+    const datos = JSON.parse(this.responseText)
+    console.log(datos)
+    $("#ServicioFormUpdate #nombre").attr("value", datos.nombre)
+    $("#ServicioFormUpdate #descripcion").attr("value", datos.descripcion)
+    $("#ServicioFormUpdate #precio").attr("value", datos.precio)
+
+    let action = "/servicios/" + datos.id
+
+    $('#ServicioFormUpdate form').attr('action', action)
+  }
+</script>
 @endsection
