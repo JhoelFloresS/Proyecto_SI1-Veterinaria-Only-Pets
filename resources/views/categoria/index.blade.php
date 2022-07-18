a32ae151f6e2ab998ae8d6d68dfcbca72e8a8926
TonyTorres <soloalaaalaz@gmail.com>
2022-07-18 11:55:45 -0400

    Terminé mi issue, siuuu

--- resources/views/categoria/index.blade.php
+++ resources/views/categoria/index.blade.php
@@ -1,12 +1,12 @@
 @extends('home')
 @section('title', 'categoria')
-@section('servicio', 'active')
+@section('petshop', 'active')
 @section('css-derecha')
     <link rel="stylesheet" href="{{ asset('css/table-information.css') }}">
     <style>
-        #ServicioFormUpdate .form-control,
-        #ServicioFormUpdate .form-select,
-        #ServicioFormUpdate .select2-selection {
+        #categoriaFormUpdate .form-control,
+        #categoriaFormUpdate .form-select,
+        #categoriaFormUpdate .select2-selection {
             background-color: khaki !important;
         }
     </style>
