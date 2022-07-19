9ae550f386aac192605945c9beffdb1fc04f4e56
MarcoToledo <marcodavidtoledo@gmail.com>
2022-07-19 00:15:38 -0400

    Botones Eliminar

--- app/Http/Controllers/CirugiaController.php
+++ app/Http/Controllers/CirugiaController.php
@@ -14,16 +14,17 @@ class CirugiaController extends Controller
         $this->middleware('can:cirugias.index')->only('index');
         $this->middleware('can:cirugias.create')->only('create', 'store');
         $this->middleware('can:cirugias.edit')->only('edit', 'update');
-
     }
 
 
-    public function index() {
+    public function index()
+    {
         $cirugias = Cirugia::get();
         return view('cirugias.index', compact('cirugias'));
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         Cirugia::create([
             'nombre' => $request->nombre,
             'tipo' => $request->tipo
@@ -31,17 +32,19 @@ class CirugiaController extends Controller
         BitacoraController::registrar(
             Auth::user()->id,
             'Creación de cirugía',
-            'Se creó la cirugia: '.$request->nombre. ' de tipo: '.$request->tipo
+            'Se creó la cirugia: ' . $request->nombre . ' de tipo: ' . $request->tipo
         );
         return redirect(route('cirugias.index'));
     }
 
-    public function datas($id){
+    public function datas($id)
+    {
         $cirugia = Cirugia::find($id);
         return $cirugia;
     }
 
-    public function update(Request $request, $id) {
+    public function update(Request $request, $id)
+    {
         $cirugia = Cirugia::findOrFail($id);
         $data = ([
             'nombre' => $request->nombre,
@@ -51,8 +54,16 @@ class CirugiaController extends Controller
         BitacoraController::registrar(
             Auth::user()->id,
             'Edición de cirugía',
-            'Se editó la cirugía: '.$request->nombre. ' de tipo: '.$request->tipo
+            'Se editó la cirugía: ' . $request->nombre . ' de tipo: ' . $request->tipo
         );
+        return redirect()->route('cirugias.index');
+    }
+
+    public function destroy($id)
+    {
+        $cirugia = Cirugia::findOrFail($id);
+        $cirugia->delete();
+
         return redirect()->route('cirugias.index');
     }
 }
--- app/Http/Controllers/VacunaController.php
+++ app/Http/Controllers/VacunaController.php
@@ -15,28 +15,37 @@ class VacunaController extends Controller
         $this->middleware('can:vacunas.index')->only('index');
         $this->middleware('can:vacunas.create')->only('create', 'store');
         $this->middleware('can:vacunas.edit')->only('edit', 'update');
-        
     }
-    
-    public function index() {
+
+    public function index()
+    {
         $vacunas = Vacuna::get();
         return view('vacunas.index', compact('vacunas'));
     }
-    
-    public function create() {
+
+    public function create()
+    {
         return view('vacunas.create');
     }
 
-    public function store(StorevacunasRequest $request) {
-        
+    public function store(StorevacunasRequest $request)
+    {
+
         VacunaAction::executeStore($request);
 
         BitacoraController::registrar(
             Auth::user()->id,
             'Creación de vacuna',
-            'Se creó la vacuna: '.$request->nombre
+            'Se creó la vacuna: ' . $request->nombre
         );
         return redirect()->route('vacunas.index');
     }
-    
+
+    public function destroy($id)
+    {
+        $vacuna = Vacuna::findOrFail($id);
+        $vacuna->delete();
+
+        return redirect()->route('vacunas.index');
+    }
 }
